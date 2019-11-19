a38c588b9c471c11962f617633682395a01147fa
Jatindera Walia <jatindera_walia@hotmail.com>
2019-11-18 20:20:50 -0600

    ng tutorial first app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    TopBarComponent,
+    CartComponent,
+    ShippingComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path: '', component: ProductListComponent },
+      {path: 'products/:productId', component: ProductDetailsComponent},
+      {path: 'cart', component: CartComponent},
+      {path: 'shipping', component: ShippingComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+    {
+        name: 'Monitor 27 Inch',
+        price: 499.99,
+        description: 'High DPI monitor'
+    },
+    {
+        name: 'Mechanical Keyboard',
+        price: 299.99,
+        description: 'Blue Keys Mechanical Keyboard',
+    },
+    {
+        name: 'Gamer Mouse',
+        price: 99.99,
+        description: 'Superior mouse in every sense',
+    },
+];
